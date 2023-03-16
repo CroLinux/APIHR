24aa44499779c9f39eb536417992c2ef35b2ef5a
CroLinux <crolinux@gmail.com>
2023-03-16 17:19:36 +0100

    Projet avec Sping Boot du cours

---
+++ APIHR/src/main/java/com/openclassrooms/apihr/ApihrApplication.java
@@ -0,0 +1,15 @@
+package com.openclassrooms.apihr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+//@PropertySource("application.properties")
+public class ApihrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApihrApplication.class, args);
+	}
+
+}
---
+++ APIHR/src/test/java/com/openclassrooms/apihr/ApihrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.apihr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApihrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
